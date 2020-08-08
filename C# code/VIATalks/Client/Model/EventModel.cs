b5b45678f5175efc86020893aa3c64eb07a2df04
Kenneth Jensen <42805394+poochy442@users.noreply.github.com>
2020-08-08 12:08:47 +0200

    Room functionality

--- C# code/VIATalks/Client/Model/EventModel.cs
+++ C# code/VIATalks/Client/Model/EventModel.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Client.Models
+namespace Client.Model
 {
     public class EventModel
     {
---
+++ C# code/VIATalks/Shared/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataClasses
+{
+    [Serializable]
+    public class Room
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public char Block { get; set; }
+        public int Capacity { get; set; }
+        public double Area { get; set; }
+        public Room(
+            int id,
+            int roomNumber,
+            char block,
+            int capacity,
+            double area)
+        {
+            Id = id;
+            RoomNumber = roomNumber;
+            Block = block;
+            Capacity = capacity;
+            Area = area;
+        }
+    }
+}
