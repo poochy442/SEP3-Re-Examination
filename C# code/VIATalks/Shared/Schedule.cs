51016e95e27fcf237097b45647f52913edc1a3af
Andrei Varzar <varzarandrew1999@gmail.com>
2020-08-11 20:28:10 +0200

    added models from 3tier
    added models from 3tier

---
+++ C# code/VIATalks/Shared/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    [Serializable]
+    class Schedule
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Schedule(int id, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
